2768850a7e63895a11133d181ebf78576328bb84
Godarck <godarck@mail.ru>
2026-05-20 13:06:14 +0300

    Update User_Setup.h for #define CYD..
     Uncomment #define CYD2_4 or #define CYD3_5 for your CYD

--- User_Setup.h
+++ User_Setup.h
@@ -222,34 +222,67 @@
 //#define TFT_BL   22  // LED back-light
 
 
-//cyd 2.4----------
-#define TFT_BL    27
-#define TFT_INVERSION_ON
-#define TOUCH_CS 33
-//cyd 2.8 ---------
+//#define CYD2_4     // Uncomment if you use 2.4 CYD
+#define CYD3_5   // Uncomment if you use 3.5 CYD
+
+//====== CYD 3.5 -----------
+#ifdef CYD3_5
+	#define ST7796_DRIVER
+	#define TFT_WIDTH  320
+	#define TFT_HEIGHT 480
+	#define TFT_BL    27
+	#define TFT_INVERSION_OFF
+	#define TFT_RGB_ORDER TFT_BGR 
+	#define TOUCH_CS 33
+
+	#define ESP32_DMA
+	#define TFT_CS    15
+	#define TFT_DC    2
+	#define TFT_RST   -1
+	#define TFT_MOSI  13
+	#define TFT_MISO  12
+	#define TFT_SCLK   14
+	#define TFT_BACKLIGHT_ON HIGH
+	#define TFT_RGB_ORDER TFT_BGR
+	#define USE_HSPI_PORT
+
+	#define SPI_FREQUENCY  80000000
+	#define SPI_READ_FREQUENCY  80000000
+	#define SPI_TOUCH_FREQUENCY  2500000
+#endif
+
+//====== CYD 2.4----------
+#ifdef CYD2_4
+	#define ILI9341_2_DRIVER
+	#define TFT_WIDTH  240
+	#define TFT_HEIGHT 320
+	#define TFT_BL    27
+	#define TFT_INVERSION_ON
+	#define TFT_RGB_ORDER TFT_BGR 
+	#define TOUCH_CS 33
+
+	#define ESP32_DMA
+	#define TFT_CS    15
+	#define TFT_DC    2
+	#define TFT_RST   -1
+	#define TFT_MOSI  13
+	#define TFT_MISO  12
+	#define TFT_SCLK   14
+	#define TFT_BACKLIGHT_ON HIGH
+	#define TFT_RGB_ORDER TFT_BGR
+	#define USE_HSPI_PORT
+
+	#define SPI_FREQUENCY  80000000
+	#define SPI_READ_FREQUENCY  80000000
+	#define SPI_TOUCH_FREQUENCY  2500000
+#endif
+//======== cyd 2.8 ---------
 //#define TFT_BL    21
 //#define TFT_INVERSION_OFF
-      
 
-#define ILI9341_2_DRIVER
-#define ESP32_DMA
-#define TFT_CS    15
-#define TFT_DC    2
-#define TFT_RST   -1
-#define TFT_MOSI  13
-#define TFT_MISO  12
-#define TFT_SCLK   14
-
-#define TFT_BACKLIGHT_ON HIGH
+//#define TFT_BACKLIGHT_ON HIGH
 //#define TFT_RGB_ORDER TFT_RGB  // Colour order Red-Green-Blue
-#define TFT_RGB_ORDER TFT_BGR  // Colour order Blue-Green-Red
-
-
-#define TFT_WIDTH  240
-#define TFT_HEIGHT 320
-#define SPI_FREQUENCY  80000000
-#define SPI_READ_FREQUENCY  80000000
-#define SPI_TOUCH_FREQUENCY  2500000
+//#define TFT_RGB_ORDER TFT_BGR  // Colour order Blue-Green-Red
 
 //#define TFT_INVERSION_ON
 //#define TOUCH_CS 21     // Chip select pin (T_CS) of touch screen
@@ -333,13 +366,13 @@
 // about 17Kbytes. To save FLASH space only enable the fonts you need!
 
 #define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
-#define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
-#define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
-#define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
-#define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
-#define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
+//#define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
+//#define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
+//#define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
+//#define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
+//#define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
 //#define LOAD_FONT8N // Font 8. Alternative to Font 8 above, slightly narrower, so 3 digits fit a 160 pixel TFT
-#define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
+//#define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
 
 // Comment out the #define below to stop the SPIFFS filing system and smooth font code being loaded
 // this will save ~20kbytes of FLASH
@@ -400,7 +433,7 @@
 // The ESP32 has 2 free SPI ports i.e. VSPI and HSPI, the VSPI is the default.
 // If the VSPI port is in use and pins are not accessible (e.g. TTGO T-Beam)
 // then uncomment the following line:
-#define USE_HSPI_PORT
+//#define USE_HSPI_PORT
 
 // Comment out the following #define if "SPI Transactions" do not need to be
 // supported. When commented out the code size will be smaller and sketches will
